3d97bd8be8bd2009cd15c88fc3390c235e9a497b
Thijnvdb <thijnvandenbergh@gmail.com>
2022-06-09 17:42:19 +0200

    added export support and size command line flag

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,32 @@ mod color;
 mod utils;
 
 use quicli::prelude::*;
-
 use clap::{Parser, ArgEnum};
+use std::io::Write;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Arguments {
     /// Input file to read
     file: String,
-    // Style to use
+    // Print mode to use
     #[clap(short, long, arg_enum)]
-    style: Option<Style>,
+    mode: Option<Mode>,
     // Color format to use
     #[clap(short, long, arg_enum)]
-    color_format: Option<ColorFormat>
+    color_format: Option<ColorFormat>,
+
+    // Directory to export to
+    #[clap(short, long)]
+    export_dir: Option<String>,
+
+    // Max size for width/height
+    #[clap(short, long)]
+    size: Option<u32>
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
-enum Style {
+enum Mode {
     Ascii,
     AsciiColored,
     Pixels
@@ -37,17 +45,19 @@ enum ColorFormat {
 }
 
 fn main() -> CliResult {
-    let mut size: u32 = 64;
-    if let Some((w, h)) = term_size::dimensions() {
-        size = std::cmp::min(w / 2, h) as u32;
-    }
+    let args: Arguments = Arguments::parse();
+    let size: u32 = if let Some(s) = args.size {s} else {
+        if let Some((w, h)) = term_size::dimensions() {
+            std::cmp::min(w / 2, h) as u32
+        } else {
+            64
+        }
+    };
 
-    let args = Arguments::parse();
     let img = image::open(&args.file)?;
-
     let resized = img.resize(size, size, image::imageops::FilterType::Nearest);
 
-    let style = if let Some(s) = args.style {s} else {Style::Pixels};
+    let mode = if let Some(s) = args.mode {s} else {Mode::Pixels};
     let color_format = if let Some(f) = args.color_format {f} else {ColorFormat::Truecolor};
 
     let color_fun: &dyn Fn(u8, u8, u8) -> String = match color_format{
@@ -55,15 +65,26 @@ fn main() -> CliResult {
         ColorFormat::Truecolor => &color::truecolor
     };
 
-    match style {
-        Style::Ascii => {
-            print::ascii_luma(&resized);
-        }
-        Style::AsciiColored => {
-            print::ascii_colored(&resized, color_fun);
-        }
-        Style::Pixels => {
-            print::filled_rgb(&resized, color_fun);
+    let output: Vec<String> = match mode {
+        Mode::Ascii => print::ascii_luma(&resized),
+        Mode::AsciiColored => print::ascii_colored(&resized, color_fun),
+        Mode::Pixels => print::filled_rgb(&resized, color_fun)
+    };
+
+    for line in &output {
+        println!("{}", line);
+    }
+
+    if let Some(out_dir) = args.export_dir {
+        if std::path::Path::new(&out_dir).is_dir() {
+            let name: &str = std::path::Path::new(&args.file).file_stem().unwrap().to_str().unwrap();
+            let mut file = std::fs::File::create(format!("{}/{}", out_dir, format!("{}.sh", name))).expect("Failed to create file");
+            let _res0 = file.write_all("#!/bin/sh\n".as_bytes());
+            for line in &output {
+                let _res = file.write_all(format!("echo \"{}\"\n", line).as_bytes());
+            }
+        } else {
+            print!("\x1b[0mExport failed: specified export directory is not a valid directory\n");
         }
     }
 
--- src/print.rs
+++ src/print.rs
@@ -2,46 +2,58 @@ use crate::ascii::get_char_from_luma;
 use crate::image::GenericImageView;
 use image::DynamicImage;
 
-pub fn ascii_luma(img: &DynamicImage) {
+pub fn ascii_luma(img: &DynamicImage) -> Vec<String> {
+    let mut lines: Vec<String> = Vec::new();
     let lum = img.to_luma8();
 
     for y in 0..img.dimensions().1 {
+        let mut line: String = "".to_string();
         for x in 0..img.dimensions().0 {
             let luma_pixel = lum.get_pixel(x,y);
             let character = get_char_from_luma(*luma_pixel);
-            print!("{}{}", character, character);
+            line.push_str(&format!("{}{}", character, character));
         }
-        print!("\n");
+        lines.push(line);
     }
+
+    return lines;
 }
 
-pub fn ascii_colored(img: &DynamicImage, color_fun: &dyn Fn(u8, u8, u8) -> String) {
+pub fn ascii_colored(img: &DynamicImage, color_fun: &dyn Fn(u8, u8, u8) -> String) -> Vec<String> {
+    let mut lines: Vec<String> = Vec::new();
     let lum = img.to_luma8();
 
     for y in 0..img.dimensions().1 {
+        let mut line: String = "".to_string();
         for x in 0..img.dimensions().0 {
             let luma_pixel = lum.get_pixel(x,y);
             let pixel = img.get_pixel(x, y);
             let character = get_char_from_luma(*luma_pixel);
             let color = color_fun(pixel[0], pixel[1], pixel[2]);
-            print!("{}{}{}", color, character, character);
+            line.push_str(&format!("{}{}{}", color, character, character));
         }
-        print!("\n");
+        lines.push(line);
     }
+
+    return lines;
 }
 
-pub fn filled_rgb(img: &DynamicImage, color_fun: &dyn Fn(u8, u8, u8) -> String) {
+pub fn filled_rgb(img: &DynamicImage, color_fun: &dyn Fn(u8, u8, u8) -> String) -> Vec<String> {
+    let mut lines: Vec<String> = Vec::new();
+    
     for y in 0..img.dimensions().1 {
+        let mut line: String = "".to_string();
         for x in 0..img.dimensions().0 {
             let pixel = img.get_pixel(x, y);
             if pixel[3] == 0 {
-                print!("  ");
+                line.push_str("  ");
             } else {
                 let color = color_fun(pixel[0], pixel[1], pixel[2]);
-                print!("{}██", color,);
+                line.push_str(&format!("{}██", color));
             }
-
         }
-        print!("\n");
+        lines.push(line);
     }
+
+    return lines;
 }
